152f339fa6b599a83da57bf5f518894612f4d96d
masakun_ <masakihikoiki@icloud.com>
2026-02-21 22:03:49 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
